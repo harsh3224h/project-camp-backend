0bc81b86e46dedee9ad29192e93cc2b4033e7d24
Harsh Kumar <harshdhiman7044@gmail.com>
2026-06-18 20:14:35 +0530

    user registration validation added

---
+++ src/validators/index.js
@@ -0,0 +1,27 @@
+import { body } from "express-validator";
+
+const userRegistrationValidator = () => {
+  return [
+    body("email")
+      .trim()
+      .notEmpty()
+      .withMessage("Email is required")
+      .isEmail()
+      .withMessage("Email is invalid"),
+
+    body("username")
+      .trim()
+      .notEmpty()
+      .withMessage("Username is required")
+      .isLowercase()
+      .withMessage("Username must be in lowercase")
+      .length({ min: 3 })
+      .withMessage("Username must be atleast 3 chracters long"),
+
+    body("password").trim().notEmpty().withMessage("Password is required"),
+
+    body("fullName").optional().trim(),
+  ];
+};
+
+export { userRegistrationValidator };
