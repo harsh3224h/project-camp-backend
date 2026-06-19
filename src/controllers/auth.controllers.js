78806c11683db846175dcc29057a7df610a1647f
Harsh Kumar <harshdhiman7044@gmail.com>
2026-06-19 07:08:52 +0530

    generateAccessAndRefreshTokens is a standalone function

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -94,8 +94,9 @@ const login = asyncHandler(async (req, res) => {
     throw new ApiError(400, "Invalid credentials");
   }
 
-  const { accessToken, refreshToken } =
-    await user.generateAccessAndRefreshTokens(user._id);
+  const { accessToken, refreshToken } = await generateAccessAndRefreshTokens(
+    user._id,
+  );
 
   const loggedInUser = await User.findById(user._id).select(
     "-password -refreshToken -emailVerificationToken -emailVerificationExpiry",
