b89be20f23447ee514399e1514e62dcafa0c0dbb
Harsh Kumar <harshdhiman7044@gmail.com>
2026-06-19 22:17:13 +0530

    attached the controllers to their corresponding routes

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,17 +1,55 @@
 import { Router } from "express";
-import { registerUser, login } from "../controllers/auth.controllers.js";
+import {
+  registerUser,
+  login,
+  logoutUser,
+  VerifyEmail,
+  RefreshAccessToken,
+  forgotPasswordRequest,
+  resetForgotPassword,
+  getCurrentUser,
+  changeCurrentPassword,
+  resendEmailVerification,
+} from "../controllers/auth.controllers.js";
 import { validate } from "../middlewares/validator.middleware.js";
 import {
   userRegistrationValidator,
   userLoginValidator,
+  userForgotPasswordValidator,
+  userResetForgotPasswordValidator,
+  userChangeCurrentPasswordValidator,
 } from "../validators/index.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
+// unprotected routes
 router
   .route("/register")
   .post(userRegistrationValidator(), validate, registerUser);
-
 router.route("/login").post(userLoginValidator(), validate, login);
+router.route("/verify-email/:verificationToken").get(VerifyEmail);
+router.route("/refresh-token").post(RefreshAccessToken);
+router
+  .route("/forgot-password")
+  .post(userForgotPasswordValidator(), validate, forgotPasswordRequest);
+router
+  .route("/reset-password/:resetToken")
+  .post(userResetForgotPasswordValidator(), validate, resetForgotPassword);
+
+// protected routes
+router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/current-user").post(verifyJWT, getCurrentUser);
+router
+  .route("/change-password")
+  .post(
+    verifyJWT,
+    userChangeCurrentPasswordValidator(),
+    validate,
+    changeCurrentPassword,
+  );
+router
+  .route("/resend-email-verification")
+  .post(verifyJWT, resendEmailVerification);
 
 export default router;
